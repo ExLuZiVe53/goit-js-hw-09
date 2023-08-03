27af55367e7558116e236597dd56e290726df80b
ExLuZiVe53 <127601818+ExLuZiVe53@users.noreply.github.com>
2023-08-03 11:42:01 +0000

    Deploying to gh-pages from @ ExLuZiVe53/goit-js-hw-09@c981b4db69bc673ab6c8ac4c276442244c2d9ef4 🚀

--- 01-color-switcher.3c15f86c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body").addEventListener("click",(function(t){(t.target!==t.target.dataset("start")||t.target.dataset("stop"))&&console.log(t.target)}));
-//# sourceMappingURL=01-color-switcher.3c15f86c.js.map
--- 01-color-switcher.ca7ae082.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body").addEventListener("click",(function(t){if(t.target!==t.target.dataset("start")||t.target.dataset("stop"))return void console.log(t.target)}));
-//# sourceMappingURL=01-color-switcher.ca7ae082.js.map
