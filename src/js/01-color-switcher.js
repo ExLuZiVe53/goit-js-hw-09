a1584d0dfef6f1f5abfa225bc0c71475276b22c3
ExLuZiVe53 <imstudent27@gmail.com>
2023-08-03 10:48:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const onBudy = document.querySelector('body');
 
+const onStart = onBudy.addEventListener('click', onClick);
+
+function onClick(event) {
+  if (
+    event.target !== event.target.dataset('start') ||
+    event.target.dataset('stop')
+  ) {
+    console.log(event.target);
+    return;
+  }
+}
