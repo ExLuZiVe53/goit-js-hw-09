c384d806b3ad2fe54397f92a744085d93930dec3
ExLuZiVe53 <127601818+ExLuZiVe53@users.noreply.github.com>
2023-08-03 13:47:23 +0000

    Deploying to gh-pages from @ ExLuZiVe53/goit-js-hw-09@a00f6401e709606280dac8fc9733d0050d9daa74 🚀

--- 01-color-switcher.9d877169.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("body button[data-start]");document.querySelector("body button[data-stop]");e.addEventListener("click",(function(){e.setAttribute("disabled","true"),r=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.backgroundColor=e}),1e3)}));var r=null;с.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled","true"),e.setAttribute("active","true")}))}();
-//# sourceMappingURL=01-color-switcher.9d877169.js.map
